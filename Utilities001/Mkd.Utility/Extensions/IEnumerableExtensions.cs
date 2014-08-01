f81616a87ba051447a1859737e7c5387401d8875
Michael Daw <michael.k.daw@outlook.com>
2014-08-01 09:04:17 -0600

    Initial commit. Added string and enumerable extensions.

---
+++ Utilities001/Mkd.Utility/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mkd.Utility
+{
+	/// <summary>
+	/// Provides extension methods on EntityCollection
+	/// </summary>
+	public static class EnumerableExtensions
+	{
+		/// <summary>
+		/// Performs the specified action on each element of the IEnumerable.
+		/// </summary>
+		/// <typeparam name="T">Generic type.</typeparam>
+		/// <param name="enumerable">The IEnumerable to iterate.</param>
+		/// <param name="action">The System.Action<typeparamref name="T"/> delegate to perform on each object in the IEnumerable<typeparamref name="T"/>./></param>
+		public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+		{
+			foreach (var item in enumerable)
+			{
+				action(item);
+			}
+		}
+
+		/// <summary>
+		/// Performs the specified action on each element of the IEnumerable. Exposes the current index of the loop.
+		/// </summary>
+		/// <typeparam name="T">Generic type.</typeparam>
+		/// <param name="enumerable">The IEnumerable to iterate.</param>
+		/// <param name="action">The System.Action<typeparamref name="T"/> delegate to perform on each object in the IEnumerable<typeparamref name="T"/>./></param>
+		/// <param name="index">Current index of the loop.</param>
+		/// <param name="startIndex">Starting index of the loop. Default is 0.</param>
+		/// <param name="doEvery">Indicates a number of items that will be skipped after the first item. Default is 1 = do every item.</param>
+		/// <param name="maxIterations">The highest index to iterate to. Default is int32.MaxValue</param>
+		public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action, ref int index, int startIndex = 0, int doEvery = 1, int maxIterations = Int32.MaxValue)
+		{
+			if (doEvery < 1)
+				doEvery = 1;
+			var enumerable1 = enumerable as T[] ?? enumerable.ToArray();
+			for (index = startIndex; index < enumerable1.Count() && index < maxIterations; index = index + doEvery)
+			{
+				action(enumerable1.ElementAt(index));
+			}
+		}
+	}
+}
---
+++ Utilities001/Mkd.Utility/Extensions/StringExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Mkd.Utility
+{
+	/// <summary>
+	/// Provides extensions for String.
+	/// </summary>
+	public static class StringExtensions
+	{
+		/// <summary>
+		/// Capitalizes the first letter of the string.
+		/// </summary>
+		/// <param name="str">The string to capitalize the first letter of.</param>
+		/// <returns>A new string equivalent to the old one but with the first letter capitalized.</returns>
+		public static string CapitalizeFirst(this string str)
+		{
+			if (str.Length > 0)
+			{
+				var first = str[0].ToString().ToUpper();
+				return str.Remove(1).Insert(0, first);
+			}
+			return str;
+		}
+
+		/// <summary>
+		/// Returns a value indicating whether a specified substring occurs within this string, in a case-insensitive manner.
+		/// </summary>
+		/// <param name="str">The string to search.</param>
+		/// <param name="value">The string to seek.</param>
+		/// <returns>true if the value parameter occurs within this string, or if value is the empty string (""); otherwise, false.</returns>
+		public static bool ContainsCaseInsensitive(this string str, string value)
+		{
+			return str.ToLower().Contains(value.ToLower());
+		}
+
+		/// <summary>
+		/// Surrounds the given string with the supplied string. Adds to beginning and end.
+		/// </summary>
+		/// <param name="str">The original string.</param>
+		/// <param name="surroundWith">String to surround the original string with.</param>
+		/// <returns>The original string surrounded by the surroundWith string.</returns>
+		public static string SurroundWith(this string str, string surroundWith)
+		{
+			return surroundWith + str + surroundWith;
+		}
+
+		/// <summary>
+		/// Tests for equivalency between two strings disregarding case.
+		/// </summary>
+		/// <param name="str">String to compare.</param>
+		/// <param name="compareString">String to compare to.</param>
+		/// <returns>True if they are the same disregarding case, false otherwise.</returns>
+		public static bool EqualsNoCase(this string str, string compareString)
+		{
+			return str.Equals(compareString, StringComparison.InvariantCultureIgnoreCase);
+		}
+	}
+}
