d058488a623977e7c28aeee475a8eb9d2931428b
michaelkdaw <michael.k.daw@outlook.com>
2014-08-08 20:18:29 -0600

    Added lower case string compare, and uploaded to nuget.

---
+++ Utilities001/Mkd.Utility/Comparers/LowerCaseComparer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mkd.Utility
+{
+    public class LowerCaseComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string x , string y)
+        {
+            return String.Equals(x , y , StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        public int GetHashCode(string obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
